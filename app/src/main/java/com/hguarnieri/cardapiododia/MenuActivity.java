ed1741e6e9c2c76e57aba5aa2987f0b920a8f6dd
Henrique Guarnieri <henrique.guarnieri@gmail.com>
2016-05-28 16:31:41 -0300

    Visual action on buttons and current day settings

--- app/src/main/java/com/hguarnieri/cardapiododia/MenuActivity.java
+++ app/src/main/java/com/hguarnieri/cardapiododia/MenuActivity.java
@@ -14,6 +14,7 @@ import com.hguarnieri.cardapiododia.fragments.MenuFragment;
 import com.hguarnieri.cardapiododia.models.Menu;
 import com.hguarnieri.cardapiododia.services.MenusService;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
@@ -25,16 +26,26 @@ public class MenuActivity extends AppCompatActivity {
 
     static boolean isLunch = true;
 
+    int currentDay = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardapio);
 
-        getMenu();
-
         this.lunchButton = (Button) findViewById(R.id.fragment_menu_lunch_button);
         this.dinnerButton = (Button) findViewById(R.id.fragment_menu_dinner_button);
 
+        createButtonListeners();
+
+        getCurrentDay();
+
+        getMenu();
+
+        getSupportActionBar().hide();
+    }
+
+    public void createButtonListeners() {
         this.lunchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,8 +61,6 @@ public class MenuActivity extends AppCompatActivity {
                 refreshScreen();
             }
         });
-
-        getSupportActionBar().hide();
     }
 
     public void refreshScreen() {
@@ -59,6 +68,45 @@ public class MenuActivity extends AppCompatActivity {
 
         this.viewMenusPager = (ViewPager) findViewById(R.id.pager);
         this.viewMenusPager.setAdapter(pagerAdapter);
+
+        this.viewMenusPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                currentDay = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
+        if (isLunch) {
+            this.lunchButton.setBackgroundResource(R.drawable.menu_selected_button);
+            this.dinnerButton.setBackgroundResource(R.drawable.menu_button);
+        } else {
+            this.dinnerButton.setBackgroundResource(R.drawable.menu_selected_button);
+            this.lunchButton.setBackgroundResource(R.drawable.menu_button);
+        }
+
+        changeDay();
+    }
+
+    public void changeDay() {
+        this.viewMenusPager.setCurrentItem(currentDay);
+    }
+
+    public void getCurrentDay() {
+        Calendar calendar = Calendar.getInstance();
+        int currentDay = calendar.get(Calendar.DAY_OF_WEEK);
+
+        // We don't have Sunday and Saturday
+        if (currentDay == 1 || currentDay == 7) {
+            this.currentDay = 0;
+        } else {
+            this.currentDay = currentDay - 1;
+        }
     }
 
     public void getMenu() {
